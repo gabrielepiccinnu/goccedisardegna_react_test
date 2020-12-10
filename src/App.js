596757c79c1730401695159da4c695d5bab278ae
Gabriele Piccinnu <gabrielepiccinnu@gmail.com>
2020-12-10 01:02:39 +0100

    Modifica pagina App.js con funzioni logiche

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import React, { useState, useEffect} from 'react';
+import './App.scss';
+import '@wordpress/components/build-style/style.css';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 
 function App() {
+  const [login, setLogin] = useState( '' );
+  const siteURL = 'yoursite_here';
+
+  useEffect(() => {
+    const localLogin = localStorage.getItem('login');
+    // If we have logged in, set it.
+    if ( localLogin ) {
+      setLogin( localLogin );
+    }
+  }, [login]);
+ 
+  return (
+    <div className="App container">
+      <h1>Headless WordPress</h1>
+      {
+        login && <Dashboard url={siteURL} token={login} setLogin={setLogin} />
+      }
+      {
+        ! login && <Login url={siteURL} setLogin={setLogin}/>
+      }
+
+    </div>
+  );
+}
+
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +51,6 @@ function App() {
       </header>
     </div>
   );
-}
+}*/
 
 export default App;
